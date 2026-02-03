dd585f7479890c44e5517964a54735b9fa1f686c
JK Prasad <prasadjk2241@gmail.com>
2026-02-03 20:17:02 +0530

    tried

--- src/main/java/com/example/FoodApp/config/AppConfig.java
+++ src/main/java/com/example/FoodApp/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 import static org.modelmapper.convention.MatchingStrategies.STRICT;
 
 @Configuration
--- src/main/java/com/example/FoodApp/config/SecurityConfig.java
+++ src/main/java/com/example/FoodApp/config/SecurityConfig.java
@@ -73,18 +73,20 @@ public class SecurityConfig {
        return configuration.getAuthenticationManager();
    }
 
-
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
 
 }
 
